33b515be6196dccea8dc9af44823cab6916a3c17
Jerry Ablan <jerryablan@dreamfactory.com>
2015-04-02 10:01:12 -0400

    Composer dependencies updated.

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * An helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.0.23 on 2015-04-01.
+ * Generated for Laravel 5.0.23 on 2015-04-02.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
