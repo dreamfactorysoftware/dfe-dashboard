c85eeb5f68a6aedc3ea111c449c313a38d22935b
Jerry Ablan <jerryablan@dreamfactory.com>
2015-04-06 12:49:15 -0400

    Current work checkpoint. See diff for details.

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * An helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.0.26 on 2015-04-03.
+ * Generated for Laravel 5.0.27 on 2015-04-06.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
