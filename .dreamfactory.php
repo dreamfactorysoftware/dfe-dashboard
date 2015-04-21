ad2a1b527e40a5f2dfa7b5e5bf3eed505e9c75ef
Jerry Ablan <jerryablan@dreamfactory.com>
2015-04-21 11:02:43 -0400

    Current work checkpoint. See diff for details.

---
+++ .dreamfactory.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * DreamFactory Enterprise(tm) Dashboard
+ * Copyright 2012-2014 DreamFactory Software, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property of DreamFactory Software, Inc. and its
+ * suppliers, if any. The intellectual and technical concepts contained herein are proprietary to DreamFactory
+ * Software, Inc. and its suppliers and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information or reproduction of this material is
+ * strictly forbidden unless prior written permission is obtained from DreamFactory Software, Inc.
+ */
