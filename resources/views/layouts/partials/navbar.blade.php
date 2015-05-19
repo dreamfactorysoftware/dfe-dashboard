26b374df0c69c1e281cf1523b61100ed0e1ed9d8
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-19 10:55:43 -0400

    Current work checkpoint. See diff for details.

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/"><img src="/img/logo-dreamfactory-inverse.png" alt=""></a>
+                <a class="navbar-brand" href="/"><img src="/img/logo-navbar-194x42.png" alt=""></a>
             </div>
 
             <div class="collapse navbar-collapse navbar-right" id="navbar-dashboard">
