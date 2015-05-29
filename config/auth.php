82ebd1ebd03f3a33fa077eb34ee6359d4ab1deb8
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-29 14:03:41 -0400

    Current work checkpoint. See diff for details.

--- config/auth.php
+++ config/auth.php
@@ -4,13 +4,13 @@
 //******************************************************************************
 
 return [
-    'driver'   => 'dashboard',
-    'model'    => 'DreamFactory\\Enterprise\\Database\\Models\\User',
-    'table'    => 'user_t',
-    'password' => [
+    'driver'            => 'dashboard',
+    'model'             => 'DreamFactory\\Enterprise\\Database\\Models\\User',
+    'table'             => 'user_t',
+    'open-registration' => true,
+    'password'          => [
         'email'  => 'dfe-common::emails.password',
         'table'  => 'auth_reset_t',
         'expire' => 60,
     ],
-
 ];
