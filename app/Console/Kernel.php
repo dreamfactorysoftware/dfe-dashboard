f75166d1199e4755ef83b6922f20520e49b7b9f3
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-04 16:19:45 -0400

    Current work checkpoint. See diff for details.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,10 +1,32 @@
 <?php namespace DreamFactory\Enterprise\Dashboard\Console;
 
+use DreamFactory\Enterprise\Common\Traits\CustomLogPath;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    //******************************************************************************
+    //* Constants
+    //******************************************************************************
+
+    /**
+     * @type string
+     */
+    const CLASS_TO_REPLACE = 'Illuminate\Foundation\Bootstrap\ConfigureLogging';
+    /**
+     * @type string
+     */
+    const REPLACEMENT_CLASS = 'DreamFactory\Enterprise\Common\Bootstrap\CommonLoggingConfiguration';
+
+    //******************************************************************************
+    //* Traits
+    //******************************************************************************
+
+    use CustomLogPath;
+
     //******************************************************************************
     //* Members
     //******************************************************************************
@@ -28,5 +50,17 @@ class Kernel extends ConsoleKernel
         /**
          * nothing scheduled yet
          */
+    }
+
+    //******************************************************************************
+    //* Methods
+    //******************************************************************************
+
+    /** @inheritdoc */
+    public function __construct( Application $app, Dispatcher $events )
+    {
+        $this->_replaceLoggingConfigurationClass( static::CLASS_TO_REPLACE, static::REPLACEMENT_CLASS );
+
+        parent::__construct( $app, $events );
     }
 }
