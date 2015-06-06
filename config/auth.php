371e6dada9bc04057ced67446f5106dc3a13e705
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-05 21:08:13 -0400

    turn registration back on after testing

--- config/auth.php
+++ config/auth.php
@@ -8,7 +8,7 @@ return [
     'model'             => 'DreamFactory\\Enterprise\\Database\\Models\\User',
     'table'             => 'user_t',
     //  If true, users may self-register. Otherwise, admins must create users */
-    'open-registration' => false,
+    'open-registration' => true,
     'password'          => [
         'email'  => 'dfe-common::emails.password',
         'table'  => 'auth_reset_t',
