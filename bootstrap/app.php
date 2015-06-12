32d8ae6e47155148ffe31429adbf760bb2bca8e1
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-12 12:30:43 -0400

    Composer dependencies updated.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,25 @@
 <?php
-/**
- * Bootstrap application
- */
-if ( !function_exists( '__bootstrap' ) )
-{
+//******************************************************************************
+//* Application Bootstrap
+//******************************************************************************
+
+if (!function_exists( '__dfe_bootstrap' )) {
     /**
      * @return \Illuminate\Foundation\Application
      */
-    function __bootstrap()
+    function __dfe_bootstrap()
     {
-        //  Create the application container
-        $_app = new Illuminate\Foundation\Application( realpath( __DIR__ . '/../' ) );
+        //  Create the app
+        $_app = new Illuminate\Foundation\Application( realpath( dirname( __DIR__ ) ) );
 
-        //  Bind bootstrap interfaces and return
+        //  Bind our default services
         $_app->singleton( 'Illuminate\Contracts\Http\Kernel', 'DreamFactory\Enterprise\Dashboard\Http\Kernel' );
         $_app->singleton( 'Illuminate\Contracts\Console\Kernel', 'DreamFactory\Enterprise\Dashboard\Console\Kernel' );
         $_app->singleton( 'Illuminate\Contracts\Debug\ExceptionHandler', 'DreamFactory\Enterprise\Dashboard\Exceptions\Handler' );
 
+        //  Return the app
         return $_app;
     }
 }
 
-return __bootstrap();
+return __dfe_bootstrap();
--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,14 +1,33 @@
 <?php
-define( 'LARAVEL_START', microtime( true ) );
+//******************************************************************************
+//* Application Autoloader
+//******************************************************************************
 
-//  Require the autoloader
-require __DIR__ . '/../vendor/autoload.php';
+if (!function_exists( '__dfe_autoload' )) {
+    define( 'LARAVEL_START', microtime( true ) );
 
-//  And pre-compiled junk
-$compiledPath = __DIR__ . '/../vendor/compiled.php';
+    /**
+     * Bootstrap DFE
+     *
+     * @return bool
+     */
+    function __dfe_autoload()
+    {
+        //  Register The Composer Auto Loader
+        $_basePath = dirname( __DIR__ );
+        require $_basePath . '/vendor/autoload.php';
 
-if ( file_exists( $compiledPath ) )
-{
-    /** @noinspection PhpIncludeInspection */
-    require $compiledPath;
+        //  Laravel 5.0
+        if (file_exists( __DIR__ . '/cache/compiled.php' )) {
+            /** @noinspection PhpIncludeInspection */
+            require __DIR__ . '/cache/compiled.php';
+        } elseif (file_exists( $_basePath . '/storage/framework/compiled.php' )) {
+            /** @noinspection PhpIncludeInspection */
+            require $_basePath . '/storage/framework/compiled.php';
+        }
+
+        return true;
+    }
 }
+
+return __dfe_autoload();
