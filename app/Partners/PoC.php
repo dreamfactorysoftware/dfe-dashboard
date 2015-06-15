5e4dd3a3ab64f6dd65c0fe31b13ff1dbb8ea602c
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-15 18:18:01 -0400

    Current work checkpoint. See diff for details.

--- app/Partners/PoC.php
+++ app/Partners/PoC.php
@@ -21,7 +21,7 @@ class PoC extends SitePartner
         $_icon = '<img src="' . $_brand->getLogo(true) . '" class="partner-brand">';
 
         return <<<HTML
-<div class="alert alert-fixed alert-dismissable well partner-well" role="alert">
+<div class="alert alert-info alert-fixed alert-dismissable partner-well" role="alert">
     <button type="button" class="close" style="padding-right: 5px;" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     <h3>{$_icon} {$this->getPartnerDetail('name')} <small>{$_brand->getCopyright($minimal)}</small></h3>
     <p>{$_brand->getCopy($minimal)}</p>
--- public/js/df.dashboard.js
+++ public/js/df.dashboard.js
@@ -4,9 +4,9 @@
  * @private
  */
 var _dso = {
-	controlForm:          null,
-	statusCheckFrequency: 30000,
-	alertHideTimeout:     30000
+    controlForm:          null,
+    statusCheckFrequency: 30000,
+    alertHideTimeout:     30000
 };
 /**
  * @type {boolean}
@@ -21,95 +21,99 @@ var _checking = false;
  * @param [show]
  * @private
  */
-var _closeAlert = function(selector, delay, show) {
-	var $_alert = jQuery(selector).alert();
+var _closeAlert = function (selector, delay, show) {
+    var $_alert = jQuery(selector).not('.alert-fixed').alert();
 
-	if (show) {
-		jQuery(selector).removeClass('hide').show();
-	}
+    if ($_alert.length) {
+        if (show) {
+            jQuery(selector).removeClass('hide').show();
+        }
 
-	window.setTimeout(function() {
-		$_alert.alert('close')
-	}, delay);
+        if (!$_alert.hasClass('alert-fixed')) {
+            window.setTimeout(function () {
+                $_alert.alert('close')
+            }, delay);
+        }
+    }
 };
 
 /**
  *
  * @private
  */
-var _checkProgress = function() {
-	(function($) {
+var _checkProgress = function () {
+    (function ($) {
 
-		$(".instance-heading-status .fa-spinner").each(function(index, item) {
-			var $_form = $("form#_dsp-control");
-			var _div = $(this).closest('a[data-toggle="collapse"]').attr('href');
+        $(".instance-heading-status .fa-spinner").each(function (index, item) {
+            var $_form = $("form#_dsp-control");
+            var _div = $(this).closest('a[data-toggle="collapse"]').attr('href');
 
-			if (_div && _div.length) {
-				var _id = $(_div).attr('id');
-				var _parts = _id.split('___');
+            if (_div && _div.length) {
+                var _id = $(_div).attr('id');
+                var _parts = _id.split('___');
 
-				if (3 == _parts.length) {
-					$('input[name="id"]', $_form).val(id);
-					$('input[name="control"]', $_form).val('status');
+                if (3 == _parts.length) {
+                    $('input[name="id"]', $_form).val(id);
+                    $('input[name="control"]', $_form).val('status');
 
-					$.post('/status/' + id, $_form.serialize(), function(data) {
-							   var $_inner = $("div#" + _id + ".panel-collapse .panel-body");
+                    $.post('/status/' + id, $_form.serialize(), function (data) {
+                            var $_inner = $("div#" + _id + ".panel-collapse .panel-body");
 
-							   $(item).fadeOut('slow', function() {
-								   $(item).fadeIn('fast');
-							   });
+                            $(item).fadeOut('slow', function () {
+                                $(item).fadeIn('fast');
+                            });
 
-							   if (data && ( 404 == data.code || 1 == data.deleted )) {
-								   //	Delete one...
-								   try {
+                            if (data && ( 404 == data.code || 1 == data.deleted )) {
+                                //	Delete one...
+                                try {
 //								_flasher($(item).closest('.accordion-group').find('.instance-heading'), true);
-									   $(item).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
-									   $(".dsp-icon i", $_inner).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
-									   $(item).closest('.panel-instance').fadeOut();
-								   } catch (e) {
-									   //	Ignore
-								   }
+                                    $(item).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
+                                    $(".dsp-icon i", $_inner).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
+                                    $(item).closest('.panel-instance').fadeOut();
+                                } catch (e) {
+                                    //	Ignore
+                                }
 
-								   _closeAlert('#alert-status-change', _dso.alertHideTimeout, true);
-							   } else {
-								   if (data.instanceState) {
-									   //	Replace message...
-									   if (data.icons[2]) {
+                                _closeAlert('#alert-status-change', _dso.alertHideTimeout, true);
+                            } else {
+                                if (data.instanceState) {
+                                    //	Replace message...
+                                    if (data.icons[2]) {
 
-										   $("div.dsp-info div.dsp-stats", $_inner).html(data.icons[2]);
-									   }
+                                        $("div.dsp-info div.dsp-stats", $_inner).html(data.icons[2]);
+                                    }
 
-									   if (data.link) {
-										   $("div.dsp-info div.dsp-name", $_inner).html(data.link);
-									   }
+                                    if (data.link) {
+                                        $("div.dsp-info div.dsp-name", $_inner).html(data.link);
+                                    }
 
-									   if (data.buttons) {
-										   $("div.dsp-info div.dsp-links .dsp-controls", $_inner).html(data.buttons);
-									   }
+                                    if (data.buttons) {
+                                        $("div.dsp-info div.dsp-links .dsp-controls", $_inner).html(data.buttons);
+                                    }
 
-									   if (data.icons[1] != 'fa-spinner fa-spin') {
+                                    if (data.icons[1] != 'fa-spinner fa-spin') {
 //								_flasher($(item).closest('.accordion-group').find('.instance-heading'));
-										   $(item).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
-										   $(".dsp-icon i", $_inner).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
+                                        $(item).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
+                                        $(".dsp-icon i", $_inner).removeClass('fa-spinner fa-spin').addClass(data.icons[1]);
 
-										   _closeAlert('#alert-status-change', _dso.alertHideTimeout, true);
-									   }
-								   }
-							   }
-						   }
+                                        _closeAlert('#alert-status-change', _dso.alertHideTimeout, true);
+                                    }
+                                }
+                            }
+                        }
 
-						, 'json');
-				}
-			}
-		});
-		_checking = false;
-	})(jQuery);
+                        , 'json');
+                }
+            }
+        });
+        _checking = false;
+    })(jQuery);
 
-	//	Re-up if there are more...
-	if (!_checking && jQuery(".instance-heading-status .fa-spinner").length) {
-		_checking = true;
-		window.setTimeout(_checkProgress, _dso.statusCheckFrequency);
-	}
+    //	Re-up if there are more...
+    if (!_checking && jQuery(".instance-heading-status .fa-spinner").length) {
+        _checking = true;
+        window.setTimeout(_checkProgress, _dso.statusCheckFrequency);
+    }
 };
 
 /**
@@ -117,27 +121,27 @@ var _checkProgress = function() {
  * @param {jQuery} $element
  * @private
  */
-var _processAction = function($element) {
-	var _id = $element.data('instance-id');
-	var _action = $element.data('instance-action');
+var _processAction = function ($element) {
+    var _id = $element.data('instance-id');
+    var _action = $element.data('instance-action');
 
-	if ('create' == _action) {
-		if (_validator && !_validator.valid()) {
-			return;
-		}
+    if ('create' == _action) {
+        if (_validator && !_validator.valid()) {
+            return;
+        }
 
-		_id = $('input#instance-name').val();
+        _id = $('input#instance-name').val();
 
-		if (!_id || !_id.length) {
-			alert('Invalid instance name.');
-		}
-	} else {
-		$element.addClass('disabled').prop('disabled', true);
-	}
+        if (!_id || !_id.length) {
+            alert('Invalid instance name.');
+        }
+    } else {
+        $element.addClass('disabled').prop('disabled', true);
+    }
 
-	var _result = _makeRequest(_id, $element.data('instance-action'), $element.data('instance-href') || null);
+    var _result = _makeRequest(_id, $element.data('instance-action'), $element.data('instance-href') || null);
 
-	$element.removeClass('disabled').prop('disabled', false);
+    $element.removeClass('disabled').prop('disabled', false);
 };
 
 /**
@@ -148,102 +152,102 @@ var _processAction = function($element) {
  * @returns {*}
  * @private
  */
-var _makeRequest = function(id, action, href) {
+var _makeRequest = function (id, action, href) {
 
-	switch (action) {
-		case 'launch':
-			window.open(href);
-			return;
-		case 'destroy':
-		case 'deprovision':
-		case 'delete':
-			if (!confirm('Permanently delete instance "' + id + '"?')) {
-				return;
-			}
-			break;
-		case 'start':
-			if (!confirm('Start instance "' + id + '"?')) {
-				return;
-			}
-			break;
-		case 'stop':
-			if (!confirm('Stop instance "' + id + '"?')) {
-				return;
-			}
-			break;
-		case 'provision':
-		case 'create':
-		case 'help':
-			break;
-		case 'export':
-			if (!confirm('Export instance "' + id + '"?')) {
-				return;
-			}
-			break;
-		case 'import':
-			//				if ( !confirm('WARNING: Destructive Procedure. This may overwrite existing settings in this DSP.' + "\n\n" + 'Import to DSP "' + id + '"?') ) {
-			//					return;
-			//				}
+    switch (action) {
+        case 'launch':
+            window.open(href);
+            return;
+        case 'destroy':
+        case 'deprovision':
+        case 'delete':
+            if (!confirm('Permanently delete instance "' + id + '"?')) {
+                return;
+            }
+            break;
+        case 'start':
+            if (!confirm('Start instance "' + id + '"?')) {
+                return;
+            }
+            break;
+        case 'stop':
+            if (!confirm('Stop instance "' + id + '"?')) {
+                return;
+            }
+            break;
+        case 'provision':
+        case 'create':
+        case 'help':
+            break;
+        case 'export':
+            if (!confirm('Export instance "' + id + '"?')) {
+                return;
+            }
+            break;
+        case 'import':
+            //				if ( !confirm('WARNING: Destructive Procedure. This may overwrite existing settings in this DSP.' + "\n\n" + 'Import to DSP "' + id + '"?') ) {
+            //					return;
+            //				}
 
-			$('input[name="id"]', _dso.controlForm).val(id);
-			$('input[name="control"]', _dso.controlForm).val('snapshots');
-			$('body').css('cursor', 'wait');
+            $('input[name="id"]', _dso.controlForm).val(id);
+            $('input[name="control"]', _dso.controlForm).val('snapshots');
+            $('body').css('cursor', 'wait');
 
-			$.post('/control', _dso.controlForm.serialize(), function(data) {
-				$('body').css('cursor', 'pointer');
-				if (data) {
-					$('#dsp-snapshot-list').html(data);
-					$('form#_dsp-snapshot-control input[name="id"]').val(id);
-					$('#dsp-import-snapshot').modal('show');
-				} else {
-					alert('No snapshots available for this instance.');
-				}
-			}, 'html');
-			return;
+            $.post('/control', _dso.controlForm.serialize(), function (data) {
+                $('body').css('cursor', 'pointer');
+                if (data) {
+                    $('#dsp-snapshot-list').html(data);
+                    $('form#_dsp-snapshot-control input[name="id"]').val(id);
+                    $('#dsp-import-snapshot').modal('show');
+                } else {
+                    alert('No snapshots available for this instance.');
+                }
+            }, 'html');
+            return;
 
-		default:
-			alert('Invalid command "' + action + '"');
-			return;
-	}
+        default:
+            alert('Invalid command "' + action + '"');
+            return;
+    }
 
-	$('input[name="id"]', _dso.controlForm).val(id);
-	$('input[name="control"]', _dso.controlForm).val(action);
+    $('input[name="id"]', _dso.controlForm).val(id);
+    $('input[name="control"]', _dso.controlForm).val(action);
 
-	return _dso.controlForm.submit();
+    return _dso.controlForm.submit();
 };
 
 /**
  * DR
  */
-jQuery(function($) {
-	//	Reusables...
-	var $_toolbars = $('div[id^="instance-toolbar-"]');
+jQuery(function ($) {
+    //	Reusables...
+    var $_toolbars = $('div[id^="instance-toolbar-"]');
 
-	//	Set form if not already
-	if (!_dso.controlForm) {
-		_dso.controlForm = $('form#_dsp-control');
-	}
+    //	Set form if not already
+    if (!_dso.controlForm) {
+        _dso.controlForm = $('form#_dsp-control');
+    }
 
-	//	Open/close toolbar
-	$_toolbars.on('show.bs.collapse', function() {
-		var $_icon = $(this).prev('.instance-actions').find('.fa-angle-down');
-		$_icon.removeClass('fa-angle-down').addClass('fa-angle-up');
-	}).on('hide.bs.collapse', function() {
-		var $_icon = $(this).prev('.instance-actions').find('.fa-angle-up');
-		$_icon.removeClass('fa-angle-up').addClass('fa-angle-down');
-	});
+    //	Open/close toolbar
+    $_toolbars.on('show.bs.collapse', function () {
+        var $_icon = $(this).prev('.instance-actions').find('.fa-angle-down');
+        $_icon.removeClass('fa-angle-down').addClass('fa-angle-up');
+    }).on('hide.bs.collapse', function () {
+        var $_icon = $(this).prev('.instance-actions').find('.fa-angle-up');
+        $_icon.removeClass('fa-angle-up').addClass('fa-angle-down');
+    });
 
-	//	No clicks allowed on disabled stuff
-	$('body').on('click', '.disabled', function(e) {
-		e.preventDefault();
-	});
+    //	No clicks allowed on disabled stuff
+    $('body').on('click', '.disabled', function (e) {
+        e.preventDefault();
+    });
 
-	//	All toolbar button clicks go here...
-	$('.panel-toolbar > button').on('click', function(e) {
-		e.preventDefault();
-		_processAction($(this));
-	});
+    //	All toolbar button clicks go here...
+    $('.panel-toolbar > button').on('click', function (e) {
+        e.preventDefault();
+        _processAction($(this));
+    });
 
-	window.setTimeout(_checkProgress, _dso.statusCheckFrequency);
-	_closeAlert('.alert-dismissable', _dso.alertHideTimeout);
+    window.setTimeout(_checkProgress, _dso.statusCheckFrequency);
+    _closeAlert('.alert-dismissable', _dso.alertHideTimeout);
 });
