f16b30f23feb52cae32dd3544a1372b5f131061a
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-17 14:41:11 -0400

    partner bar changes

--- config/partner.php
+++ config/partner.php
@@ -4,26 +4,33 @@
  */
 return [
     'vz' => [
-        'name'          => 'Verizon',
-        'class'         => 'DreamFactory\\Enterprise\\Partner\\AlertPartner',
-        'referrers'     => ['verizon.com', 'hubspot.com', 'dreamfactory.com'],
-        'commands'      => ['utc_post'],
-        'description'   => 'Verizon Wireless',
-        'alert-context' => 'alert-info',
-        //'redirect-uri' => 'https://my.cloud.verizon.com/registration/#/',
-        'brand'         => [
-            'logo'      => '/vendor/dfe-partner/assets/img/logo-944x702.png',
-            'icon'      => '/vendor/dfe-partner/assets/img/logo-135x100.png',
+        'name' => 'Verizon',
+        'class' => 'DreamFactory\\Enterprise\\Dashboard\\Partners\\Verizon',
+        'referrers' => ['verizon.com', 'hubspot.com', 'dreamfactory.com'],
+        'commands' => ['utc_post'],
+        'description' => 'Verizon Wireless',
+        'alert-context' => 'alert-danger',
+        'redirect-uri' => 'https://my.cloud.verizon.com/registration/#/',
+        'brand' => [
+            'action' => <<< HTML
+HTML
+            ,
+            'logo' => '/vendor/dfe-partner/assets/img/logo-944x702.png',
+            'icon' => '/vendor/dfe-partner/assets/img/logo-135x100.png',
             'copyright' => '&copy; ' . date('Y') . ' Verizon',
-            'copy'      => <<< HTML
-<h3>Why not move your instance to a shiny new Verizon Cloud? Click the button below to be swept away...</h3>
-    <form method="POST" action="/partner/vz" class="pull-right">
-   		<input type="hidden" name="_token" value="__CSRF_TOKEN__">
-        <input type="hidden" name="command" value="utc_post">
-
-        <button type="submit" class="btn btn-success">Read <strong>More</strong>...</button></p>
-    </form>
-    <div style="clear: both"></div>
+            'copy' => <<< HTML
+<div class="row partner-row">
+    <div class="col-md-2 col-sm-3" style="padding-left: 0;"><img src="/vendor/dfe-partner/assets/img/logo-135x100.png" class="partner-brand"></div>
+    <div class="col-md-8 col-sm-6 partner-copy""><h3>Deploy DreamFactory to your own server hosted on the Verizon cloud.</h3></div>
+    <div class="col-md-2 col-sm-3">
+        <form method="POST" action="/partner/vz" class="pull-right">
+            <input type="hidden" name="_token" value="__CSRF_TOKEN__">
+            <input type="hidden" name="command" value="utc_post">
+            <button type="submit" class="btn btn-danger partner-button">Sign Up Now!</button>
+        </form>
+        <div style="clear: both"></div>
+    </div>
+</div>
 HTML
             ,
         ],
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Oswald|Montserrat' rel='stylesheet' type='text/css'>
     <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/partner.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
     <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
@@ -21,20 +22,20 @@
     @section('head-scripts')
     @show
     <script type="text/javascript">
-    var _rcCallback = function() {
-        var _gr = {
-            'sitekey': '{{ config('recaptcha.siteKey') }}',
-            'theme':   'light'
+        var _rcCallback = function () {
+            var _gr = {
+                'sitekey': '{{ config('recaptcha.siteKey') }}',
+                'theme': 'light'
+            };
+
+            $('.g-recaptcha').each(function () {
+                var _id = $(this).attr('id');
+
+                if (_id) {
+                    grecaptcha.render(_id, _gr);
+                }
+            });
         };
-
-        $('.g-recaptcha').each(function() {
-            var _id = $(this).attr('id');
-
-            if (_id) {
-                grecaptcha.render(_id, _gr);
-            }
-        });
-    };
     </script>
     <script src="https://www.google.com/recaptcha/api.js?onload=_rcCallback&render=explicit" async defer></script>
 </head>
@@ -52,22 +53,23 @@
             <ul class="list-inline">
                 <li>
                     <a target="_blank"
-                        href="http://facebook.com/dfsoftwareinc"><i class="fa fa fa-facebook-square fa-2x"></i></a>
+                       href="http://facebook.com/dfsoftwareinc"><i class="fa fa fa-facebook-square fa-2x"></i></a>
                 </li>
                 <li>
                     <a target="_blank"
-                        href="https://twitter.com/dfsoftwareinc"><i class="fa fa-twitter-square fa-2x"></i></a>
+                       href="https://twitter.com/dfsoftwareinc"><i class="fa fa-twitter-square fa-2x"></i></a>
                 </li>
                 <li>
                     <a target="_blank"
-                        href="http://dreamfactorysoftware.github.io/"><i class="fa fa-github-square fa-2x"></i></a>
+                       href="http://dreamfactorysoftware.github.io/"><i class="fa fa-github-square fa-2x"></i></a>
                 </li>
             </ul>
         </div>
         <div class="clearfix"></div>
         <p>
             <span class="pull-left hidden-xs hidden-sm">{!! config( 'dfe.common.display-name' ) !!}
-                <small style="margin-left: 5px;font-size: 9px;">({!! config('dfe.common.display-version') !!})</small></span>
+                <small style="margin-left: 5px;font-size: 9px;">({!! config('dfe.common.display-version') !!})
+                </small></span>
             <span class="pull-right">{!! config('dfe.common.display-copyright') !!}</span>
         </p>
     </div>
