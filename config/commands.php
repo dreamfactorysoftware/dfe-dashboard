c8e0ffa2720e5b00fde23a1a2b46e152b447878b
Jerry Ablan <jerryablan@dreamfactory.com>
2015-12-07 11:53:03 -0500

    release/1.0.1

--- config/commands.php
+++ config/commands.php
@@ -7,6 +7,6 @@ return [
     //* Information displayed when running commands
     //******************************************************************************
     'display-name'      => 'DreamFactory Enterprise(tm) Dashboard',
-    'display-version'   => 'v1.0.0',
+    'display-version'   => 'v1.0.1',
     'display-copyright' => 'Copyright (c) 2012-' . date('Y') . ', All Rights Reserved',
 ];
