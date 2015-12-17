009ff1b60bb46ba02746165609fb328b8197d0bb
Jerry Ablan <jerryablan@dreamfactory.com>
2015-12-17 16:26:55 -0500

    Added customized error message for registration

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,22 +1,11 @@
 <?php
-
 return [
-
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
-
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user" => "We can't find a user with that e-mail address.",
-	"token" => "This password reset token is invalid.",
-	"sent" => "We have e-mailed your password reset link!",
-	"reset" => "Your password has been reset!",
-
+    //******************************************************************************
+    //* Password related messages
+    //******************************************************************************
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'user'     => 'The combination of credentials entered is not valid.',
+    'token'    => 'This password reset token is invalid.',
+    'sent'     => 'An password reset link has been sent to your email address.',
+    'reset'    => 'Your password has been reset.',
 ];
