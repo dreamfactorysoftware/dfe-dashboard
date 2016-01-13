c4afcd60a33442c8c1b0bd937258b5803193f3c7
Jerry Ablan <jerryablan@dreamfactory.com>
2016-01-13 15:57:13 -0500

    Added upload manager service

--- config/dashboard.php
+++ config/dashboard.php
@@ -35,6 +35,7 @@ return [
         ServerTypes::WEB => 'success',
         ServerTypes::APP => 'warning',
     ],
+    'upload-store'             => 'uploads',
     //******************************************************************************
     //* General UI Alert Settings
     //******************************************************************************
