077a5ef53d71b877685a12a03730b8525e60dc5c
Jerry Ablan <jerryablan@dreamfactory.com>
2016-03-08 20:40:40 -0500

    Composer dependencies updated.

--- config/app.php
+++ config/app.php
@@ -21,7 +21,7 @@ return [
         /** Laravel Framework Service Providers... */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
+        Collective\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
